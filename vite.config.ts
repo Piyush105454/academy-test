b8edf14b91bc878207358841b5b7507dc47ad72a
Piyush105454 <Piyushmodi812@gmail.com>
2026-08-09 19:27:38 +0530

    update volunter id

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,7 @@ export default defineConfig({
   server: {
     host: "0.0.0.0",
     port: 8080,
+    allowedHosts: ["academy.wazireducationsociety.org", "localhost", "127.0.0.1"],
     hmr: {
       overlay: false,
     },
