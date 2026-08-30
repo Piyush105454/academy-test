cf2b38ac11dfbc6c10a246696cb4f8f39e9e4aec
Piyush105454 <Piyushmodi812@gmail.com>
2026-08-30 20:36:57 +0530

    updatedtask bulk

--- scratch/fix_student_auth.cjs
+++
@@ -1,6 +0,0 @@
-const fs = require('fs');
-
-let studentAuth = fs.readFileSync('src/pages/StudentAuth.tsx', 'utf-8');
-studentAuth = studentAuth.replace(/\} catch \(err\) \{ \{/g, "} catch (err) {");
-fs.writeFileSync('src/pages/StudentAuth.tsx', studentAuth);
-console.log('Fixed StudentAuth.tsx');
